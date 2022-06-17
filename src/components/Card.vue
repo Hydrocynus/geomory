f475c7f47c019bca5f82131a906118e386291b13
Hydrocynus <tobias.ulmer@itc-studenten.de>
2022-06-17 11:57:47 +0200

    Beispiel + -ESLint

---
+++ src/components/Card.vue
@@ -0,0 +1,13 @@
+<script setup>
+  
+</script>
+
+<template>
+  <div>
+    CARD
+  </div>
+</template>
+
+<style>
+  
+</style>
---
+++ src/views/Menu.vue
@@ -0,0 +1,17 @@
+<script setup>
+  import { Card } from '../components/Card.vue'
+  import { ref } from 'vue'
+
+  const data = ref([0,1,2,3,6,5])
+</script>
+
+<template>
+  <div>
+    <Card></Card>
+    <span v-for="x in data"><span v-if="x % 2 === 0">{{ x }}</span></span>
+  </div>
+</template>
+
+<style>
+
+</style>
