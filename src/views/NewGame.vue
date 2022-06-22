612b573ba0a7cbc6027becd53171f9530d25326c
heyimsolace <justin.derks01@gmail.com>
2022-06-22 09:19:03 +0200

    New Game Working

--- src/views/NewGame.vue
+++ src/views/NewGame.vue
@@ -2,12 +2,14 @@
   import { ref } from 'vue'
   import { Player } from '@/classes/Player'
   import { PlayerChamber } from '@/classes/PlayerChamber'
+import router from '@/router';
 
   const chamber = ref(new PlayerChamber())
-  const cardCnt = ref(0)
+  const cardCnt = ref(4)
+  const maxPlayers = 10
 
   function addPlayer() {
-    if (chamber.value.players.length < 10) {
+    if (chamber.value.players.length < maxPlayers) {
       const player = new Player('')
       chamber.value.addPlayer(player)
       console.debug(player, chamber.players)
@@ -19,23 +21,37 @@
     console.debug(player, chamber.players)
   }
 
+
+  function submit() {
+    chamber.value.players = chamber.value.players.filter(player => player.name.length > 0)
+
+    if (chamber.value.players.length < maxPlayers) {
+      document.getElementById("notEnoughPlayersMSG").style.display = "block"
+      return
+    }
+
+    router.push({path: "/game" ,cardCnt: cardCnt, chamber: chamber})
+
+  }
+
 </script>
 
 <template>
-  <h3>Cards:</h3>
-  <input v-model="cardCnt" placeholder="Card Count" />
 
-  <h3>Players: {{ chamber.players.length }} / 10</h3>
-  <button @click="addPlayer()"><i class="fa-solid fa-user-plus"></i></button>
-  <div v-for="player in chamber.players">
-    <input v-model="player.name" placeholder="Playername"/>
+  <h2 id="notEnoughPlayersMSG" style="display: none">You gotta have at least a single player, bro</h2>
+
+  <h3>Cards: {{ cardCnt }}</h3>
+  <input type="range" min="4" max="50" step="2" v-model.number="cardCnt" placeholder="Card Count" />
+
+  <h3>Players: {{ chamber.players.length }} / {{ maxPlayers }}</h3>
+  <button @click="addPlayer()">Add Player <i class="fa-solid fa-user-plus"></i></button>
+  <div class="playerDisplayDiv" v-for="player in chamber.players">
+    <input v-model.trim="player.name" placeholder="Playername"/>
     <button @click="removePlayer(player)"><i class="fa-solid fa-user-minus"></i></button>
   </div>
 
   <br />
-  <router-link :cardCnt="cardCnt" :chamber="chamber" to="/game">
-    <button><i class="fa-solid fa-location-arrow"></i></button>
-  </router-link>
+    <button @click="submit()">Start Game <i class="fa-solid fa-location-arrow"></i></button>
 </template>
 
 <style>
