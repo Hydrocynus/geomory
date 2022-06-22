56dc292768c91640a9f45b85fd7e05397ac644be
Hydrocynus <tobias.ulmer@itc-studenten.de>
2022-06-22 09:55:21 +0200

    Game added

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Menu from '../views/Menu.vue'
 import NewGame from '../views/NewGame.vue'
+import Game from '../views/Game.vue'
 
 const routes = [
   { path: '/', component: Menu },
   { path: '/newgame', component: NewGame },
+  { path: '/game', component: Game },
 ]
 
 const router = createRouter({
---
+++ src/views/Game.vue
@@ -0,0 +1,19 @@
+<script setup>
+  import { computed } from '@vue/reactivity';
+  import { ref } from 'vue'
+
+  const props = defineProps({ chamber: Array, cardCnt: Number })
+  // const activePlayer = computed(() => props.chamber.find(c => c.active))
+
+  console.debug(props, props.chamber, props.cardCnt)
+</script>
+
+<template>
+  <div>
+    <span>Active Player: {{ activePlayer }}</span>
+  </div>
+</template>
+
+<style>
+  
+</style>
