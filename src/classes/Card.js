08fcf541f94c38edcee95ecc49276c78f75e01f4
Hydrocynus <tobias.ulmer@itc-studenten.de>
2022-06-29 12:28:34 +0200

    Bug fixes

--- src/classes/Card.js
+++ src/classes/Card.js
@@ -2,12 +2,12 @@ export class Card {
   static maxID = 0;
 
   constructor (url, city, matches, id) {
-    this.id = id || Card.maxID +1;
+    this.id = id ?? Card.maxID +1;
     Card.maxID = Math.max(Card.maxID, this.id);
     this.matches = matches;
     this.url = url;
     this.city = city;
+    this.visible = false;
+    this.matched = false;
   }
-
-  
 }
