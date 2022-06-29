a5a5f2ac50ca720f9b42300958824a337a542507
timodue <timo.duengel@itc-stundenten.de>
2022-06-29 11:36:42 +0200

    menue

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -1,16 +1,20 @@
 <script setup>
-  import { ref } from 'vue'
-
-  const data = ref([0,1,2,3,6,5])
+  import { ref } from 'vue'  
 </script>
-
 <template>
-  <div>
-    <Card></Card>
-    <span v-for="x in data"><span v-if="x % 2 === 0">{{ x }}</span></span>
-  </div>
+
+    <router-link to="/newgame"> New Game <i class="fa-solid fa-gamepad"> </i> 
+    </router-link> 
+    
+    <br>
+    <br>
+
+    <router-link to="/continuegame"> Continue Game <i class="fa-solid fa-play"> </i>
+
+    </router-link> 
 </template>
 
+  
 <style>
 
 </style>
