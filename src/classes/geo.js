8b9d432b2f73d1c0b5ec038d8994bc5813e32305
timodue <timo.duengel@itc-stundenten.de>
2022-06-29 14:02:05 +0200

    Menu und Geo klasse für daten

---
+++ src/classes/geo.js
@@ -0,0 +1,35 @@
+class geo {
+
+  static location = [
+    {id: 0, matches: 1, city: "Dortmund", url: "public/img/geo/DAF-Bahnhofsvorplatz-1.jpg"},
+    {id: 1, matches: 0, city: "Dortmund", url: "public/img/geo/DAF-Bahnhofsvorplatz-2.jpg"},
+    {id: 2, matches: 3, city: "Dortmund", url: "public/img/geo/DAF-Corsopassage-1.jpg"},
+    {id: 3, matches: 2, city: "Dortmund", url: "public/img/geo/DAF-Corsopassage-2.jpg"},
+    {id: 4, matches: 5, city: "Dortmund", url: "public/img/geo/DAF-Dortberghaus-1.jpg"},
+    {id: 5, matches: 4, city: "Dortmund", url: "public/img/geo/DAF-Dortberghaus-2.jpg"},
+    {id: 6, matches: 7, city: "Dortmund", url: "public/img/geo/DAF-Hansaplatz-Arkaden-2a.jpg"},
+    {id: 7, matches: 6, city: "Dortmund", url: "public/img/geo/DAF-Hansaplatz-Arkaden-2b.jpg"},
+    {id: 8, matches: 9, city: "Dortmund", url: "public/img/geo/DAF-Hauptbahnhof-1.jpg"},
+    {id: 9, matches: 10, city: "Dortmund", url: "public/img/geo/DAF-Hauptbahnhof-2.jpg"},
+    {id: 10, matches: 11, city: "Dortmund", url: "public/img/geo/DAF-Karstadt-Sport.jpg"},
+    {id: 11, matches: 10, city: "Dortmund", url: "public/img/geo/DAF-Karstadt-Sporthaus-2.jpg"},
+    {id: 12, matches: 13, city: "Dortmund", url: "public/img/geo/DAF-Körnerplatz(Westentor)-1.jpg"},
+    {id: 13, matches: 12, city: "Dortmund", url: "public/img/geo/DAF-Körnerplatz(Westentor)-2.jpg"},
+    {id: 14, matches: 15, city: "Dortmund", url: "public/img/geo/DAF-Ostwallmuseum(Oberbergamt)-1.jpg"},
+    {id: 15, matches: 14, city: "Dortmund", url: "public/img/geo/DAF-Ostwallmuseum(Oberbergamt)-2.jpg"},
+    {id: 16, matches: 17, city: "Dortmund", url: "public/img/geo/DAF-Propsteikirche-1.jpg"},
+    {id: 17, matches: 16, city: "Dortmund", url: "public/img/geo/DAF-Propsteikriche-2.jpg"},
+    {id: 18, matches: 19, city: "Dortmund", url: "public/img/geo/DAF-Reinoldikirche-1.jpg"},
+    {id: 19, matches: 18, city: "Dortmund", url: "public/img/geo/DAF-Reinoldikirche-2.jpg"},
+    {id: 20, matches: 21, city: "Dortmund", url: "public/img/geo/DAF-Stadtthaus-1.jpg"},
+    {id: 21, matches: 20, city: "Dortmund", url: "public/img/geo/DAF-Stadthaus-2.jpg"},
+    {id: 22, matches: 23, city: "Dortmund", url: "public/img/geo/DAF-Stadttheater-1.jpg"},
+    {id: 23, matches: 22, city: "Dortmund", url: "public/img/geo/DAF-Stadttheater-2.jpg"},
+    {id: 24, matches: 25, city: "Dortmund", url: "public/img/geo/DAF-Stahlhaus(Löwenhof)-1.jpg"},
+    {id: 25, matches: 24, city: "Dortmund", url: "public/img/geo/DAF-Stahlhaus(Löwenhof)-2.jpg"},
+    {id: 26, matches: 27, city: "Dortmund", url: "public/img/geo/DAF-Synagoge(Platz-der-Synagoge)-1.jpg"},
+    {id: 27, matches: 26, city: "Dortmund", url: "public/img/geo/DAF-Synagoge(Platz-der-Synagoge)-2.jpg"},
+
+  ]
+    
+}
--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -1,10 +1,19 @@
 <script setup>
-  import { ref } from 'vue'
+  import { computed } from '@vue/reactivity';
 
-  const data = ref([0,1,2,3,6,5])
+
+  const lS = computed(() =>localStorage.getItem("geomory"));
 </script>
 
 <template>
+
+  <h1>Geomory</h1>
+  <br>
+
+  <img src="../assets/logo.svg"/>
+
+  <br>
+  <br>
 
     <router-link to="/newgame"> New Game <i class="fa-solid fa-gamepad"> </i> 
     </router-link> 
@@ -12,11 +21,23 @@
     <br>
     <br>
 
-    <router-link to="/continuegame"> Continue Game <i class="fa-solid fa-play"> </i>
+
+    <router-link to="/continuegame" v-if="ls != null"> Continue Game <i class="fa-solid fa-play"> </i>
 
     </router-link> 
 </template>
 
+
 <style>
+
+/* https://colorhunt.co/palette/eeeddee0ddaa203239141e27 */
+
+h1{
+  color: #42b983;
+
+}
+img{
+  width:225px;
+}
 
 </style>
