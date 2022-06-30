8959be10156ac3f85d7f1c9e5535a170990cbf40
Hydrocynus <tobias.ulmer@itc-studenten.de>
2022-06-30 02:04:31 +0200

    added method "shuffle" to Array-Class

--- src/main.js
+++ src/main.js
@@ -6,3 +6,15 @@ import '@fortawesome/fontawesome-free/js/fontawesome.min.js'
 import '@fortawesome/fontawesome-free/js/solid.min.js'
 
 createApp(App).use(router).mount('#app')
+
+// Prototypes
+
+Object.defineProperty(Array.prototype, 'shuffle', {
+  value: function() {
+      for (let i = this.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [this[i], this[j]] = [this[j], this[i]];
+      }
+      return this;
+  }
+});
