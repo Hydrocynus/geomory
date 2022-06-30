8c762e65276025717207fd368f6585bfc750911f
Hydrocynus <tobias.ulmer@itc-studenten.de>
2022-06-30 02:03:44 +0200

    added continue game site to router

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Menu from '../views/Menu.vue'
 import NewGame from '../views/NewGame.vue'
 import Game from '../views/Game.vue'
+import Continue from '../views/Continue.vue'
 
 const routes = [
   { path: '/', name: "home", component: Menu },
+  { path: '/continue', name: "continue", component: Continue },
   { path: '/newgame', name: "newgame", component: NewGame },
   { path: '/game', name: "game", component: Game, props: true },
 ]
